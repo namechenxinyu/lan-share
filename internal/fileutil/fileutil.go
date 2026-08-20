a45811d1740b1e3798c54aa162480238901ecb6e
Mr-ChenXY <namechenxinyu@163.com>
2026-08-20 14:07:40 +0800

    fix: correct Windows filename sanitizer return value

--- internal/fileutil/fileutil.go
+++ internal/fileutil/fileutil.go
@@ -43,7 +43,7 @@ func sanitizeWindowsName(name string) string {
 	case "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9":
 		name = "_" + name
 	}
-	return name, nil
+	return name
 }
 
 func UniqueDestination(dir, name string) string {
